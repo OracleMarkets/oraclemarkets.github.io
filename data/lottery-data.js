471be6f9efd0e073e2ec3a5266c80c809f197af8
OracleMarkets <adrianadrnlee@gmail.com>
2026-06-14 03:18:51 +1000

    Added current lottery

--- data/lottery-data.js
+++ data/lottery-data.js
@@ -1,1 +1,1 @@
-window.LOTTERY_DATA = {"pot":0,"entryUrl":"https://crestconomy.com/guild/oracle/store","currentLottery":"","history":[]};
+window.LOTTERY_DATA = {"pot":0,"entryUrl":"https://crestconomy.com/guild/oracle/store","currentLottery":"Oracle Daily Lottery Ticket | Day 2","history":[]};
